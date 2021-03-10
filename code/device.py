e160941df5ed3ed18ce191f527266dca10b0642d
Prometheus <prometheus3375@gmail.com>
2021-03-10 23:18:26 +0300

    Add \n to device print

--- code/device.py
+++ code/device.py
@@ -1,4 +1,4 @@
 import torch
 
 Device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'PyTorch is using {Device.type.upper()}')
+print(f'PyTorch is using {Device.type.upper()}\n')
