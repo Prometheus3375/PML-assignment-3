6a6aa2b6ba54485076aad8da0f918f29ae39af18
Prometheus <prometheus3375@gmail.com>
2021-03-12 01:28:22 +0300

    Make model more independent
    Added RNN.hd which is (bi + 1) * hidden_dim.
    Update all comments using new params.
    hyper -> H.

---
+++ code/H.py
@@ -0,0 +1,21 @@
+dataset_slice = slice(10_000)  # in sentences
+ru_word_count_minimum = 1
+en_word_count_minimum = 1
+infrequent_words_percent = 0.01
+batch_size = 30
+
+encoder_bi = True
+encoder_hidden_dim = 256
+encoder_hd = (encoder_bi + 1) * encoder_hidden_dim
+encoder_embed_dim = 256
+
+decoder_hidden_dim = 256
+decoder_hd = decoder_hidden_dim
+decoder_embed_dim = 256
+
+learning_rate = 0.001
+gradient_norm_clip = 1.
+
+epochs = 10
+
+teaching_percent = 0.5
