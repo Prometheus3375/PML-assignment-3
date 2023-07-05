89a6ffaba4c6197a32bb4f5a26bd3b33d16fe687
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:08:01 +0300

    ...

--- hyper.py
+++ hyper.py
@@ -2,5 +2,5 @@ dataset_slice = slice(10_000)  # in sentences
 infrequent_words_percentage = 0.02
 hidden_state_size = 256
 learning_rate = 0.001
-log_interval = 10  # in words
+log_interval = 10  # in batches
 teaching_percentage = 0.5
