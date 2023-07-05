8eee75193abfd201c3c5a8f8982bdb8b1dcddb33
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:08:05 +0300

    Update hyper
    Added hypers to control min word count.

--- code/hyper.py
+++ code/hyper.py
@@ -1,6 +1,8 @@
-dataset_slice = slice(1_000)  # in sentences
+dataset_slice = slice(100_000)  # in sentences
+ru_word_count_minimum = 2
+en_word_count_minimum = 2
 infrequent_words_percent = 0.01
-batch_size = 100
+batch_size = 8
 
 embed_dim = 256
 hidden_dim = 256
@@ -8,7 +10,7 @@ hidden_dim = 256
 learning_rate = 0.001
 norm_clip = 1.
 
-epochs = 10
+epochs = 20
 log_interval = 10  # in batches
 
 teaching_percent = 0.5
