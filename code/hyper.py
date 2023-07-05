493b0c203f575af0219fcc317b8be5b9777a42f9
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:08:04 +0300

    Update train
    Criterion uses sum as reduction.
    Added epochs.

--- code/hyper.py
+++ code/hyper.py
@@ -1,11 +1,13 @@
-dataset_slice = slice(100_000)  # in sentences
+dataset_slice = slice(1_000)  # in sentences
 infrequent_words_percentage = 0.01
 batch_size = 10
 
-embed_dim = 100
+embed_dim = 256
 hidden_dim = 256
 
 learning_rate = 0.001
+
+epochs = 20
 log_interval = 10  # in batches
 
 teaching_percentage = 0.5
