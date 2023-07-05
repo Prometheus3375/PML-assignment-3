42011fdcf86d7e230dabe749fbea39c0c0004c3d
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:08:00 +0300

    PML #3 - update test
    Fixed arguments.
    Added printer and time evaluation.
    Renamings.
    Fixed encoder.init_hidden().

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ from torch import Tensor
 
 from data import EOS, Language, Token_EOS, Token_SOS, preprocess_ru
 from device import Device
+from misc import Printer, time
 from model import AttnDecoderRNN, EncoderRNN
 
 
 def evaluate(encoder: EncoderRNN, decoder: AttnDecoderRNN, input_tensor: Tensor):
     with torch.no_grad():
         input_length = input_tensor.size()[0]
-        encoder_hidden = encoder.initHidden()
+        encoder_hidden = encoder.init_hidden()
 
         encoder_outputs = torch.zeros(decoder.max_length, encoder.hidden_size, device=Device)
 
@@ -37,23 +38,9 @@ def evaluate(encoder: EncoderRNN, decoder: AttnDecoderRNN, input_tensor: Tensor)
         return decoded_indexes
 
 
-if __name__ == '__main__':
-    default_model = 'data.pth'
-
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser(
-        usage='Runs model on test.ru.txt and save results to answer.zip with answer.txt',
-    )
-    parser.add_argument(
-        'model',
-        help=f'path to data file. Default: {default_model!r}',
-        default=default_model,
-    )
-
-    args = parser.parse_args()
-
-    hidden_state_size, max_output_length, ru_counter, en_counter, encoder_w, decoder_w = torch.load(args.model)
+@time
+def main(data_path: str):
+    hidden_state_size, max_output_length, ru_counter, en_counter, encoder_w, decoder_w = torch.load(data_path)
 
     ru_lang = Language(ru_counter)
     en_lang = Language(en_counter)
@@ -68,12 +55,42 @@ if __name__ == '__main__':
 
     with open('test.ru.txt') as f:
         with open('answer.txt', 'w') as out:
-            for line in f:
-                sentence = preprocess_ru(line)
-                ru = ru_lang.sentence2tensor(f'{sentence} {EOS}').view(-1, 1)
+            with Printer() as printer:
+                printer.print(f'Testing: starting...')
+                lines = f.readlines()
 
-                answer = evaluate(encoder, decoder, ru)
-                out.write(' '.join(en_lang.index2word[i] for i in answer) + '\n')
+                total = len(lines)
+                for i, line in enumerate(lines, 1):
+                    sentence = preprocess_ru(line)
+                    ru = ru_lang.sentence2tensor(f'{sentence} {EOS}').view(-1, 1)
+
+                    answer = evaluate(encoder, decoder, ru)
+                    out.write(' '.join(en_lang.index2word[i] for i in answer) + '\n')
+
+                    if i % 10 == 0:
+                        printer.print(f'Testing: {i / total:.0%} [{i:,}/{total:,}]')
+
+                printer.print(f'Testing: completed')
 
     with ZipFile('answer.zip', 'w') as z:
         z.write('answer.txt')
+
+
+if __name__ == '__main__':
+    default_data = 'data.pth'
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        usage='Runs model on test.ru.txt and save results to answer.zip with answer.txt',
+    )
+    parser.add_argument(
+        'data',
+        nargs='?',
+        default=default_data,
+        help=f'path to data file. Default: {default_data!r}',
+    )
+
+    args = parser.parse_args()
+
+    main(args.data)
